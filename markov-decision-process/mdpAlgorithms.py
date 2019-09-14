d0f2ce959c539e7331f961efef4e2106f1d89453
Harsh Siloiya <harshsiloiya@gmail.com>
2019-09-14 12:32:59 +0530

    Added code for linear programming

--- markov-decision-process/mdpAlgorithms.py
+++ markov-decision-process/mdpAlgorithms.py
@@ -14,7 +14,36 @@ def run_algorithm(numStates, numActions, rewards, transition, discount, mdpType,
 # finds optimal value function using linear programming
 def LinearProgramming(numStates, numActions, rewards, transition, discount, mdpType):
     mdpProblem = pulp.LpProblem("MDP", pulp.LpMinimize)
-    return 0
+    dictVar = pulp.LpVariable.dict("value_function", range(numStates))
+    # adding objective function
+    mdpProblem += pulp.lpSum([dictVar[i] for i in range(numStates)])
+    # adding constraints
+    for state1 in range(numStates):
+        for action in range(numActions):
+            lowerBound = 0
+            for state2 in range(numStates):
+                lowerBound += transition[state1][action][state2] * (rewards[state1][action][state2] + discount * dictVar[state2])
+            mdpProblem += dictVar[state1] >= lowerBound
+    # additional constraint for episodic MDPs
+    if (mdpType == "episodic"):
+        mdpProblem += (dictVar[numStates - 1] == 0)
+    # solve the linear programming problem
+    mdpProblem.solve()
+    V = np.zeros((numStates, 1))
+    for i in range(numStates):
+        V[i][0] = dictVar[i].varValue
+    # getting the optimum policy
+    P = np.zeros((numStates, 1), dtype = int)
+    tmp = np.zeros((numActions, ))
+    for state in range(numStates):
+        for action in range(numActions):
+            t = np.reshape(transition[state][action], (numStates, 1))
+            r = np.reshape(rewards[state][action], (numStates, 1))
+            r = r + discount * V
+            result = np.matmul(np.transpose(t), r)
+            tmp[action] = result[0][0]
+        P[state][0] = np.argmax(tmp)
+    return P, V
 
 # finds optimal value function using policy iteration
 def PolicyIteration(numStates, numActions, rewards, transition, discount, mdpType):
